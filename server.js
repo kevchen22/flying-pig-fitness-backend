61f646d5aa02113dc7c612dca145aa04838c474d
kevchen22 <kchen012197@gmail.com>
2023-01-12 14:21:14 -0500

    changing port

--- server.js
+++ server.js
@@ -29,6 +29,4 @@ app.use('/api/v1/foodlog', ensureLoggedIn, require('./routes/api/foodLog'))
 app.use('/api/v1/home', ensureLoggedIn)
 
 // ===== PORT =====
-const PORT =  process.env.PORT || 8080
-
-app.listen(PORT, () => console.log(`Express app running on port ${PORT}`))
+app.listen(process.env.PORT || 8080);
